a615afa2f797eca0ad190fc52d8f24b91573e94d
crysislinux <crysislinux@163.com>
2015-09-21 15:10:44 +0800

    ignore some libraries when parsed by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,14 @@ var webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   path = require('path'),
   srcPath = path.join(__dirname, 'src'),
-  nodePath = path.join(__dirname, 'node_modules');
+  nodeModulesPath = path.join(__dirname, 'node_modules');
 
-module.exports = {
+var deps = [
+  'react/dist/react.min.js',
+  'alt/dist/alt.min.js'
+];
+
+var config = {
   target: 'web',
   cache: true,
   entry: {
@@ -15,8 +20,9 @@ module.exports = {
   },
   resolve: {
     // in case of something in srcPath named same as modules in node_modules, search nodePath first, then srcPath.
-    root: [nodePath, srcPath],
-    extensions: ['', '.js', '.css']
+    root: [nodeModulesPath, srcPath],
+    extensions: ['', '.js', '.css'],
+    alias: {}
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -30,7 +36,8 @@ module.exports = {
       {test: /\.js?$/, exclude: /node_modules/, loader: 'babel?cacheDirectory'},
       {test: /\.css$/, exclude: /node_modules/, loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'},
       {test: /\.png$/, exclude: /node_modules/, loader: 'file?name=./assets/image/[hash].[ext]'}
-    ]
+    ],
+    noParse: []
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
@@ -48,3 +55,11 @@ module.exports = {
     historyApiFallback: true
   }
 };
+
+deps.forEach(function (dep) {
+  var depPath = path.resolve(nodeModulesPath, dep);
+  config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+  config.module.noParse.push(depPath);
+});
+
+module.exports = config;
